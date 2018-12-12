8686ea0b747d4476a271196c49b1235048b5a68c
almei <almei@LAPTOP-EPIOIF7H>
2018-12-12 19:58:42 +0000

    Version 2 Kata4 modelo CIPO

--- Kata4/src/main/kata4.java
+++ Kata4/src/main/kata4.java
@@ -13,10 +13,25 @@ public class kata4 {
     static Histogram c;
     
     public static void main(String[] args) throws IOException{
+    	input();
+        process();
+        output();
+    }
+    
+
+    
+    public static void input() throws IOException{
         MailListReader a = new MailListReader();
         listMails = a.read("C:\\Users\\Armando\\Desktop\\a.txt");
+    }
+    
+    public static void process(){
         MailHistogramBuilder b = new MailHistogramBuilder();
         c = b.build(listMails);
+
+    }
+    
+    public static void output(){
         new HistogramDisplay(c).execute();
     }
 }
